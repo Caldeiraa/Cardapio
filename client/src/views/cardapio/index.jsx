a2c6deb9e15e45f92c18544e8f5e95cf161b51e1
Caldeiraa <mirilo.bxd@gmail.com>
2025-04-11 17:53:42 -0300

    atualizaçoes

--- client/src/views/cardapio/index.jsx
+++ client/src/views/cardapio/index.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import Lanche from '../../img/hamburguer.png'
+import Espetinho from '../../img/espetinho.png'
+import Sorvete from '../../img/sorvete.png'
+import Combo from '../../img/combo.png'
+import './cardapio.css'
 
-function index() {
+function Index() {
   return (
     <div>
-      <h1>ola mundo</h1>
+      <div className="titulo">
+        <h1>Cardápio - MuriloBotique</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-6 d-flex justify-content-center mb-4">
+            <img className="img-um" src={Lanche} alt="Lanche" />
+          </div>
+          <div className="col-6 d-flex justify-content-center mb-4">
+            <img className="img-dois" src={Espetinho} alt="Espetinho" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6 d-flex justify-content-center mb-4">
+            <img className="img-tres" src={Sorvete} alt="Sorvete" />
+          </div>
+          <div className="col-6 d-flex justify-content-center mb-4">
+            <img className="img-tres" src={Combo} alt="Combo" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default index
+export default Index
